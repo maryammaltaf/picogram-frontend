167d7f6d37b20aac756053a8d49b30643ae45aa7
emmvi <maryam.altaf.10@gmail.com>
2021-11-15 22:14:10 +0500

    first commit

---
+++ src/components/login/login.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react"
+import "./login.css"
+import axios from "axios"
+import { useHistory } from "react-router-dom"
+
+import { FaUserAlt, FaKey, FaFacebookF, FaTwitter, FaLinkedinIn, FaGoogle } from "react-icons/fa";
+//import blue from './blue.png'
+import logo from './picogramlogo.png'
+import bg from './bg2.png'
+import vector from './vector.png'
+
+const Login = ({ setLoginUser }) => {
+
+    const history = useHistory()
+
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+    })
+
+    const handleChange = e => {
+        const { name, value } = e.target
+        // console.log(name, value)
+        setUser({
+            ...user, //modifies one and others remain same
+            [name]: value
+        })
+    }
+
+    const login = () => {
+        axios.post("http://localhost:9002/login", user)
+            .then(res => {
+                alert(res.data.message)
+                setLoginUser(res.data.user)
+                history.push("/")
+            })
+    }
+
+    return (
+        
+        // <div className="login">
+        //     {console.log("User", user)}
+        //     <h1>Login</h1>
+        //     <input type="text" name="email" value={user.email} onChange={handleChange} placeholder="Enter your Email"></input>
+        //     <input type="password" name="password" value={user.password} onChange={handleChange} placeholder="Enter your Password"></input>
+        //     <div className="button" onClick={login}>Login</div>
+        //     <div>or</div>
+        //     <div className="button" onClick={() => history.push("/register")}>Register</div>
+        // </div>
+
+        <div class="container">
+            <img src={bg} class="bg" />
+
+            <div class="form-container">
+                <div className="login">
+                    <img src={vector} class="vector" />
+                    {console.log("User", user)}
+                    <form action="" class="sign-in-form">
+                        <img src={logo} class="logo" />
+                        <h2 class="title">Sign In</h2>
+                        <div class="input-field" >
+                            <FaUserAlt > </FaUserAlt>
+                            <input type="text" name="email" value={user.email} onChange={handleChange} placeholder="Email"></input>
+                        </div>
+                        <div class="input-field">
+                            <FaKey ></FaKey>
+                            <input type="password" name="password" value={user.password} onChange={handleChange} placeholder="Password"></input>
+                        </div>
+                        
+                        <input type="submit" value="Login" class="btn solid" onClick={login} />
+
+                        <p class="social-text"> Or sign in with social platforms</p>
+                        <div class="social-media">
+                            <a href="#" class="social-icon">
+                                <FaFacebookF></FaFacebookF>
+                            </a>
+                            <a href="#" class="social-icon">
+                                <FaGoogle></FaGoogle>
+                            </a>
+                            <a href="#" class="social-icon">
+                                <FaLinkedinIn></FaLinkedinIn>
+                            </a>
+                            <a href="#" class="social-icon">
+                                <FaTwitter></FaTwitter>
+                            </a>
+                        </div>
+
+                        <input type="submit" value="Don't have an account? Sign Up?" class="signup" onClick={() => history.push("/register")} />
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default Login
